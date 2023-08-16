c6d0d35f03cb51925b41502ca0d54255cc84b621
Naresh Kumar <banvro07@gmail.com>
2023-08-16 14:19:18 +0530

    to do project using tkinter

---
+++ to-do-app.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class TodoApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("To-Do List App")
+        self.root.geometry("400x400")
+
+        self.tasks = []
+
+        self.task_entry = tk.Entry(root, font=("Helvetica", 16))
+        self.task_entry.pack(pady=20)
+
+        self.add_button = tk.Button(root, text="Add Task", command=self.add_task)
+        self.add_button.pack()
+
+        self.task_listbox = tk.Listbox(root, font=("Helvetica", 14), selectmode=tk.SINGLE)
+        self.task_listbox.pack(padx=20, pady=10, fill=tk.BOTH, expand=True)
+
+        self.remove_button = tk.Button(root, text="Remove Task", command=self.remove_task)
+        self.remove_button.pack()
+
+        self.load_tasks()
+
+    def add_task(self):
+        task = self.task_entry.get()
+        if task:
+            self.tasks.append(task)
+            self.update_task_listbox()
+            self.save_tasks()
+            self.task_entry.delete(0, tk.END)
+
+    def remove_task(self):
+        selected_index = self.task_listbox.curselection()
+        if selected_index:
+            index = selected_index[0]
+            del self.tasks[index]
+            self.update_task_listbox()
+            self.save_tasks()
+
+    def update_task_listbox(self):
+        self.task_listbox.delete(0, tk.END)
+        for task in self.tasks:
+            self.task_listbox.insert(tk.END, task)
+
+    def load_tasks(self):
+        try:
+            with open("tasks.txt", "r") as file:
+                self.tasks = [line.strip() for line in file.readlines()]
+            self.update_task_listbox()
+        except FileNotFoundError:
+            pass
+
+    def save_tasks(self):
+        with open("tasks.txt", "w") as file:
+            for task in self.tasks:
+                file.write(task + "\n")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = TodoApp(root)
+    root.mainloop()
